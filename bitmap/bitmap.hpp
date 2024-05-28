d13718391eec1c16b44fc63bd0e2468babdd1a0a
Thinker-Maker <130532493+Thinker-Maker@users.noreply.github.com>
2024-05-28 22:19:20 +0800

    bitset

---
+++ bitmap/bitmap.hpp
@@ -0,0 +1,8 @@
+//
+// Created by 董鸿博 on 2024/5/24.
+//
+
+#ifndef BITMAP_BITMAP_HPP
+#define BITMAP_BITMAP_HPP
+
+#endif //BITMAP_BITMAP_HPP
---
+++ bitmap/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "bitmap.hpp"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
