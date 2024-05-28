664fda162d7b93ea4c3b4f600ff6ea77e7565531
Thinker-Maker <130532493+Thinker-Maker@users.noreply.github.com>
2024-05-28 22:20:35 +0800

    Delete bitmap directory

--- bitmap/bitmap.hpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by 董鸿博 on 2024/5/24.
-//
-
-#ifndef BITMAP_BITMAP_HPP
-#define BITMAP_BITMAP_HPP
-
-#endif //BITMAP_BITMAP_HPP
--- bitmap/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "bitmap.hpp"
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
